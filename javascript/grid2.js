34cb9a7326dbe7df1556375f37eb255ae4db89d0
Luqmon Fathiat <luqmonfathiatmorenikeji@gmail.com>
2023-10-16 18:44:57 +0100

    edit on js

--- javascript/grid2.js
+++ javascript/grid2.js
@@ -1,12 +1,39 @@
+// var person = () => {
+//     console.log("Hello")
+// }
+function Question() {
+    colInput = document.createElement("input")
+    colInput.type = "number"
+    colLabel = document.createElement("label")
+    colLabel.innerText = "Number Of Columns"
+    body = document.body;
+    body.appendChild(colLabel)
+    body.appendChild(colInput)
+    colLabel.for = "yes"
+    colInput.id = "yes"
+    colInput.placeholder = "Enter number of columns"
+    console.log(colLabel.for)
+}
+var Inputs = {
+    numberOfColumns: 8,
+    firstColor: "Burlywood",
+    secondColor: "Brown",
+    gapSize: "1px",
+    changeDiagonalColor: "Darkred"
+}
+
+
 function grid () {
     userInput = document.querySelector("#userInput")
     userColor1 = document.querySelector("#userFirstColor")
     userColor2 = document.querySelector("#userSecondColor")
     userGapSize = document.querySelector("#userGapSize")
+    userDiagonalColor = document.querySelector("#colorChanging")
     squareSize = '60px';
     gapSize = userGapSize.value;
     lightColor = userColor1.value
     darkColor = userColor2.value
+    diagColor = userDiagonalColor.value
     fullName = []
     colNo = userInput.value;
     numberOfColumn = Math.floor(colNo);
@@ -15,7 +42,10 @@ function grid () {
     text = document.body;
     userValue = userInput.value
     container = document.createElement("div");
-    container.style.display = 'grid';
+    container.style.display = 'grid';    
+
+
+
     for(var j = 0; j < numberOfColumn; j++) {
         fullName.push(squareSize)
     }
@@ -40,11 +70,19 @@ function grid () {
         }
         container.appendChild(elements)
     }
+    var children = container.children
+    for(i=0;i<numberOfColumn;i++){
+        toChange = (i + (numberOfColumn*i))
+        children[toChange].style.backgroundColor = diagColor
+    }
     emptySpace = document.createElement('p')
     emptySpace.innerText = " "
     text.appendChild(container)
     text.appendChild(emptySpace)
     num = 3.2;
-    console.log(userColor1)
+    // console.log(children.length)
 }
+
+// person
 // grid();
+Question();
